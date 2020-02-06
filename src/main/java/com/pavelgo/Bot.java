2da2675a8b9a833f07310728cf9e1e2502ad4cb4
PavelGozhii <pavelgozhii@gmail.com>
2020-02-06 23:25:18 +0300

    Update

--- src/main/java/com/pavelgo/Bot.java
+++ src/main/java/com/pavelgo/Bot.java
@@ -43,6 +43,7 @@ public class Bot extends TelegramLongPollingBot {
         }
     }
 
+
     public synchronized void sendMsg(String chatId, String s) {
         SendMessage sendMessage = new SendMessage();
         setButtons(sendMessage);
